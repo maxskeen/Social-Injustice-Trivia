a2caf1b521be4396370b0a18f05664b8ba379a75
maxskeen <69686704+maxskeen@users.noreply.github.com>
2020-11-08 03:34:23 -0500

    Add files via upload

--- Screen.py
+++ Screen.py
@@ -1,44 +1,52 @@
 from tkinter import *
 
-class Screen:
+class Screens:
 
-    def __init__(self, root, qNum):
+    nextPressed = False
+
+    def __init__(self, qNum):
+        
+        self.root = Tk()
 
         #Set Screen size
-        root.geometry("1920x1080")
-        root.title("SOCIAL INJUSTICE TRIVIA")
+        self.root.geometry("{0}x{1}+0+0".format(self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
+        #self.root.geometry("1920x1080")
+        self.root.title("SOCIAL INJUSTICE TRIVIA")
 
         self.qNum = qNum
 
         #Declare Answer Choice Buttons
-        self.btn1 = Button(root, text = "Hfef", command = self.clicked, height = 25, width = 25)
-        self.btn1.place(x = 500, y = 100)
-        #self.btn1.pack()
+        self.btn1 = Button(self.root, text = "Button 1", command = self.clicked, height = 5, width = 25)
+        self.btn1.place(x = 500, y = 575)
 
-        self.btn2 = Button(root, text = "fe")
-        self.btn2.pack()
-        self.btn3 = Button(root, text = "ef")
-        self.btn3.pack()
-        self.btn4 = Button(root, text = "ef")
-        self.btn4.pack()
+        self.btn2 = Button(self.root, text = "Button 2", command = self.clicked, height = 5, width = 25)
+        self.btn2.place(x = 750, y = 575)
+
+        self.btn3 = Button(self.root, text = "Button 3", command = self.clicked, height = 5, width = 25)
+        self.btn3.place(x = 500, y = 650)
+
+        self.btn4 = Button(self.root, text = "Button 4", command = self.clicked, height = 5, width = 25)
+        self.btn4.place(x = 750, y = 650)
 
         #Declare Next Button
-        self.nextButton = Button(root, text = "")
-        self.nextButton.pack()
+        self.nextButton = Button(self.root, text = "Next", height = 5, width = 10, command = self.nextScreen)
 
         #Declare Header Label
-        self.headerLabel = Label(root, text = "Question: " + str(qNum))
-        self.headerLabel.pack(side = "top")
+        self.headerLabel = Label(self.root, text = "Question: " + str(qNum), bg = "blue", height = 5, width = 75)
+        self.headerLabel.place(x = 425, y = 100)
 
         #Declare Question Label
-        self.questionLabel = Label(root, text = '')
-        self.questionLabel.pack()
+        self.questionLabel = Label(self.root, text = 'HELLO', height = 5, width = 75, bg = "blue")
+        self.questionLabel.place(x = 425, y = 450)
 
         #Main loop
-        root.mainloop()
+        self.root.mainloop()
 
     def clicked(self):
         print("I click")
+        self.nextButton.place(x = 750, y = 650)
+        self.nextButton.pack()
 
-
-    #d = {"1800": False, "1900": True}
+    def nextScreen(self):
+        nextPressed = True
+        self.root.destroy()
